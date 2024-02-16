87bb8f72be21a149263d9cc6c0a92f4571ccea1e
Frank <fha@easv.dk>
2024-02-16 18:05:11 +0100

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        WebShop ws = new WebShop();
+        ws.buyStuff();
+		
+		//Add magic here, step 3
+        //Something about testing the webshopUI with an enum?
+
+
+        //For step 5
+        //PurchaseConfirmation pc = new PurchaseConfirmation();
+        //confirmationEmail call?
+    }
+
+}
---
+++ src/main/java/PurchaseConfirmation.java
@@ -0,0 +1,20 @@
+public class PurchaseConfirmation {
+
+    private ShoppingCart shoppingCart = null;   //Step 5, how can this be set?
+
+    /**
+     * Prints all the product descriptions
+     * And the Total purchase price
+     * Maybe the class is missing a ShoppingCart reference?
+     */
+    public void confirmationEmail()
+    {
+		//Add magic here, step 5
+        double totPrice = 0;
+
+        //System.out.println("Description: " + <eachproduct>.getDescription());
+
+        System.out.println("Purchase Price : " + totPrice);
+    }
+
+}
